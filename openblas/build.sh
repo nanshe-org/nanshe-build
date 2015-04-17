13fa9284510b57fb98de519a64232f965c22abee
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-17 13:21:27 -0400

    openblas/build.sh: Make links to the openblas lib.

--- openblas/build.sh
+++ openblas/build.sh
@@ -1,3 +1,10 @@
+# Depending on our platform, shared libraries end with either .so or .dylib
+if [[ `uname` == 'Darwin' ]]; then
+    DYLIB_EXT=dylib
+else
+    DYLIB_EXT=so
+fi
+
 if [[ `uname` == 'Darwin' ]]; then
     CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
     CXX_LDFLAGS="${LDFLAGS} -stdlib=libc++"
@@ -10,3 +17,6 @@ fi
 
 make TARGET=NEHALEM BINARY=64 NO_LAPACK=0 NO_AFFINITY=1 NUM_THREADS=1 -j${CPU_COUNT}
 make install PREFIX=$PREFIX
+
+ln -fs $PREFIX/lib/libopenblas.$DYLIB_EXT $PREFIX/lib/libblas.$DYLIB_EXT
+ln -fs $PREFIX/lib/libopenblas.$DYLIB_EXT $PREFIX/lib/liblapack.$DYLIB_EXT
