d5c9471764b6d4ed3bc6114d24104cd86ed9c798
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-16 23:30:19 -0400

    scipy: Make it work.

--- scipy/build.sh
+++ scipy/build.sh
@@ -3,12 +3,21 @@
 #export BLAS=$PREFIX/lib/libf77blas.so
 #export ATLAS=$PREFIX/lib/libatlas.so
 
-export LAPACK=$PREFIX/lib/libopenblas.so
-export BLAS=$PREFIX/lib/libopenblas.so
+# Depending on our platform, shared libraries end with either .so or .dylib
+if [[ `uname` == 'Darwin' ]]; then
+    DYLIB_EXT=dylib
+else
+    DYLIB_EXT=so
+fi
+
+export LAPACK=$PREFIX/lib/libopenblas.$DYLIB_EXT
+export BLAS=$PREFIX/lib/libopenblas.$DYLIB_EXT
 
 test -f $LAPACK
 test -f $BLAS
 #test -f $ATLAS
+
+unset LDFLAGS
 
 $PYTHON setup.py install
 
