d16382671725492c491865e6b1a94bbc9916302d
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-16 22:00:30 -0400

    openblas: Make it work.

--- openblas/build.sh
+++ openblas/build.sh
@@ -1,2 +1,12 @@
+if [[ `uname` == 'Darwin' ]]; then
+    CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+    CXX_LDFLAGS="${LDFLAGS} -stdlib=libc++"
+    #CXXFLAGS="${CXXFLAGS}"
+    #CXX_LDFLAGS="${LDFLAGS}"
+else
+    CXXFLAGS="${CXXFLAGS}"
+    CXX_LDFLAGS="${LDFLAGS}"
+fi
+
 make TARGET=NEHALEM BINARY=64 NO_LAPACK=0
 make install PREFIX=$PREFIX
