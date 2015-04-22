d5582e6709cedb3940606c86210f9590ce3b8346
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-22 16:20:46 -0400

    numpy: Build with clang on mac to avoid TLS emutls linking bug ( https://gcc.gnu.org/bugzilla/show_bug.cgi?id=39888 ).

--- numpy/build.sh
+++ numpy/build.sh
@@ -6,8 +6,12 @@
 # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ `uname` == 'Darwin' ]]; then
     DYLIB_EXT=dylib
+    CC=clang
+    CXX=clang++
 else
     DYLIB_EXT=so
+    CC=gcc
+    CXX=g++
 fi
 
 export LAPACK=$PREFIX/lib/libopenblas.$DYLIB_EXT
@@ -24,5 +28,5 @@ test -f $BLAS
 unset LDFLAGS
 
 #LDFLAGS="${LDFLAGS} -lpython2.7 -c" $PYTHON setup.py install
-$PYTHON setup.py install
+CC=$CC CXX=$CXX $PYTHON setup.py install
 
