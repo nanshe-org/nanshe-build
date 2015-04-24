c2f84f103caf14895e0f7f7b3e36d4c48967079c
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-23 23:17:22 -0400

    bottleneck/build.sh: Use clang on Mac if possible.

--- bottleneck/build.sh
+++ bottleneck/build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Depending on our platform, shared libraries end with either .so or .dylib
+if [[ `uname` == 'Darwin' ]]; then
+    DYLIB_EXT=dylib
+    CC=clang
+    CXX=clang++
+else
+    DYLIB_EXT=so
+    CC=gcc
+    CXX=g++
+fi
+
 mkdir -vp ${PREFIX}/bin;
 
 export CFLAGS="-Wall -g -m64 -pipe -O2 -march=x86-64 -fPIC"
@@ -7,5 +18,5 @@ export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 
-${PYTHON} setup.py install || exit 1;
+CC=$CC CXX=$CXX $PYTHON setup.py install || exit 1;
 
