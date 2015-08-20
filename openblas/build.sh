0be0a0e0bd22eaf75a7e4ba42f464558f67b64dc
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-20 11:19:23 -0400

    openblas/build.sh: Add some comments to explain what we are doing.

--- openblas/build.sh
+++ openblas/build.sh
@@ -5,6 +5,7 @@ else
     DYLIB_EXT=so
 fi
 
+# On Mac, make sure that we use libc++.
 if [[ `uname` == 'Darwin' ]]; then
     CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
     CXX_LDFLAGS="${LDFLAGS} -stdlib=libc++"
@@ -13,11 +14,17 @@ else
     CXX_LDFLAGS="${LDFLAGS}"
 fi
 
+# Build all CPU targets and allow dynamic configuration
+# Build LAPACK.
+# Set number of thread to 1; however, this can be changed by
+# setting OPENBLAS_NUM_THREADS before loading the library.
 make DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 NUM_THREADS=1 -j${CPU_COUNT}
 make install PREFIX=$PREFIX
 
+# As OpenBLAS, now will have all symbols that BLAS or LAPACK have,
+# create libraries with the standard names that are linked back to
+# OpenBLAS. This will make it easier for packages that are looking for them.
 ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/libblas.a
 ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/liblapack.a
-
 ln -fs $PREFIX/lib/libopenblas.$DYLIB_EXT $PREFIX/lib/libblas.$DYLIB_EXT
 ln -fs $PREFIX/lib/libopenblas.$DYLIB_EXT $PREFIX/lib/liblapack.$DYLIB_EXT
