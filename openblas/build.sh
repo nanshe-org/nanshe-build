488914c202482f8bfb31e966e4c584899a9bda7c
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-20 11:18:16 -0400

    openblas/build.sh: Skip setting compilers as it is no longer necessary.

--- openblas/build.sh
+++ openblas/build.sh
@@ -1,12 +1,8 @@
 # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ `uname` == 'Darwin' ]]; then
     DYLIB_EXT=dylib
-    CC=clang
-    CXX=clang++
 else
     DYLIB_EXT=so
-    CC=gcc
-    CXX=g++
 fi
 
 if [[ `uname` == 'Darwin' ]]; then
@@ -19,7 +15,7 @@ else
     CXX_LDFLAGS="${LDFLAGS}"
 fi
 
-CC=$CC CXX=$CXX make DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 NUM_THREADS=1 -j${CPU_COUNT}
+make DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 NUM_THREADS=1 -j${CPU_COUNT}
 make install PREFIX=$PREFIX
 
 ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/libblas.a
