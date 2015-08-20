5c5ceb310cb1c862baa0af6d689102694b0f387f
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-20 19:45:32 -0400

    openblas/build.sh: Change `libopenblas.dylib`'s paths to refer to the included libraries for gfortran and the like. Required as building with the provided `gfortran` does not work.

--- openblas/build.sh
+++ openblas/build.sh
@@ -12,6 +12,16 @@ fi
 make DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 NUM_THREADS=1 -j${CPU_COUNT}
 make install PREFIX=$PREFIX
 
+# Make sure the linked gfortran libraries are searched for on the RPATH.
+if [[ `uname` == 'Darwin' ]]; then
+    GFORTRAN_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libgfortran.3.dylib)\"))")
+    GCC_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libgcc_s.1.dylib)\"))")
+    QUADMATH_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libquadmath.0.dylib)\"))")
+    install_name_tool -change $GFORTRAN_LIB @rpath/libgfortran.3.dylib $PREFIX/lib/libopenblas.dylib
+    install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $PREFIX/lib/libopenblas.dylib
+    install_name_tool -change $QUADMATH_LIB @rpath/libquadmath.0.dylib $PREFIX/lib/libopenblas.dylib
+fi
+
 # As OpenBLAS, now will have all symbols that BLAS or LAPACK have,
 # create libraries with the standard names that are linked back to
 # OpenBLAS. This will make it easier for packages that are looking for them.
