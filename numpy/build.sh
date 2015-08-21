9ea0454ac9d977586735fc6ff2d80cc4f3eabaf2
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-21 07:19:21 -0400

    numpy/build.sh: Check each library to make sure it is properly linked.

--- numpy/build.sh
+++ numpy/build.sh
@@ -9,7 +9,9 @@ $PYTHON setup.py install
 # Make sure the linked gfortran libraries are searched for on the RPATH.
 if [[ `uname` == 'Darwin' ]]; then
     GCC_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libgcc_s.1.dylib)\"))")
-    install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $SP_DIR/numpy/linalg/_umath_linalg.so
-    install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $SP_DIR/numpy/linalg/lapack_lite.so
-    install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $SP_DIR/numpy/core/_dotblas.so
+
+    for EACH_FILE in `find ${SP_DIR}/numpy -name "*.so"`;
+    do
+        install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $EACH_FILE
+    done
 fi
