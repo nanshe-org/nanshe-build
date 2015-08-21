f3c573e777efd7574149df1a57aaa1ec7bc3023b
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-21 07:43:17 -0400

    scipy/build.sh: Patch any remaining references to the gfortran compiler.

--- scipy/build.sh
+++ scipy/build.sh
@@ -5,3 +5,13 @@ unset LDFLAGS
 
 $PYTHON setup.py install
 
+
+# Make sure the linked gfortran libraries are searched for on the RPATH.
+if [[ `uname` == 'Darwin' ]]; then
+    GCC_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libgcc_s.1.dylib)\"))")
+
+    for EACH_FILE in `find ${SP_DIR}/scipy -name "*.so"`;
+    do
+        install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $EACH_FILE
+    done
+fi
