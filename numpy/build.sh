facdb2d1406d55c280cedf0a2b4ad68d6ab2d188
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-20 23:48:47 -0400

    numpy/build.sh: Patch some paths that don't point to the right library.

--- numpy/build.sh
+++ numpy/build.sh
@@ -5,3 +5,11 @@ unset LDFLAGS
 
 $PYTHON setup.py install
 
+
+# Make sure the linked gfortran libraries are searched for on the RPATH.
+if [[ `uname` == 'Darwin' ]]; then
+    GCC_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libgcc_s.1.dylib)\"))")
+    install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $SP_DIR/numpy/linalg/_umath_linalg.so
+    install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $SP_DIR/numpy/linalg/lapack_lite.so
+    install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $SP_DIR/numpy/core/_dotblas.so
+fi
