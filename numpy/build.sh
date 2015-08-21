cd7cc38d1c9b6dbf9a5a97eb7d81a10d57c3bb64
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-20 23:47:25 -0400

    numpy/build.sh: Skip setting specific compilers or setting variables related to the BLAS. NumPy seems to pick up on these fine.

--- numpy/build.sh
+++ numpy/build.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
 
-# Depending on our platform, shared libraries end with either .so or .dylib
-if [[ `uname` == 'Darwin' ]]; then
-    DYLIB_EXT=dylib
-    CC=clang
-    CXX=clang++
-else
-    DYLIB_EXT=so
-    CC=gcc
-    CXX=g++
-fi
-
-export LAPACK=$PREFIX/lib/libopenblas.$DYLIB_EXT
-export BLAS=$PREFIX/lib/libopenblas.$DYLIB_EXT
-
-test -f $LAPACK
-test -f $BLAS
 
 unset LDFLAGS
 
-CC=$CC CXX=$CXX $PYTHON setup.py install
+$PYTHON setup.py install
 
