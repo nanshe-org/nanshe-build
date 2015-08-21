890b486cb0b38fd195e01f5f7f1be145b02b6f95
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-20 23:44:14 -0400

    numpy/build.sh: Drop commented code.

--- numpy/build.sh
+++ numpy/build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-#export LAPACK=$PREFIX/lib/liblapack.so
-#export BLAS=$PREFIX/lib/libf77blas.so
-#export ATLAS=$PREFIX/lib/libatlas.so
 
 # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ `uname` == 'Darwin' ]]; then
@@ -19,14 +16,8 @@ export BLAS=$PREFIX/lib/libopenblas.$DYLIB_EXT
 
 test -f $LAPACK
 test -f $BLAS
-#test -f $ATLAS
-
-#export BLAS=None
-#export LAPACK=None
-#export ATLAS=None
 
 unset LDFLAGS
 
-#LDFLAGS="${LDFLAGS} -lpython2.7 -c" $PYTHON setup.py install
 CC=$CC CXX=$CXX $PYTHON setup.py install
 
